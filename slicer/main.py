bf1e2fc84ceb2b543b6bb258eff421defcac6e72
John Phillips <john.h.phillips@clegadgetworks.com>
2016-03-25 12:07:00 -0700

    Started parser

--- slicer/main.py
+++ slicer/main.py
@@ -1,5 +1,36 @@
-'''
-Created on Mar 24, 2016
+ # open file
+ file = open(input_name, 'r')
+ # empty list of rows
+ rows = []
+      
+ for line in file:
+    # empty row of attributes
+    row = []
+        
+    current_row = line.split(',')
+    
+    # find lat / long for given CSV file in header row
+    if line == 0:
+        for label in current_row:
+            if label == "latitude":
+                lat = 
+            if label == "longitude"
 
-@author: jphilips
-'''
+    
+        
+    lat = current_row[0]
+    row.append(lat)
+    lon = current_row[1]
+    row.append(lon)
+    depth = current_row[3]
+    row.append(depth)
+    salinity = current_row[6]
+    row.append(salinity)
+    temperature = current_row[5]
+    row.append(temperature)
+    sound_speed = current_row[7]
+    row.append(sound_speed)
+        
+    rows.append(row)
+        
+file.close()
